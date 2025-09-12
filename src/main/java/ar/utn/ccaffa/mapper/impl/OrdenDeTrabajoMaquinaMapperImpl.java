becdaacec6bc4beb296d6438f3cc2f06182f6f08
juanignaciog <juanignaciog400@gmail.com>
2025-09-11 21:35:56 -0300

    fix en mapper

--- src/main/java/ar/utn/ccaffa/mapper/impl/OrdenDeTrabajoMaquinaMapperImpl.java
+++ src/main/java/ar/utn/ccaffa/mapper/impl/OrdenDeTrabajoMaquinaMapperImpl.java
@@ -19,7 +19,7 @@ public class OrdenDeTrabajoMaquinaMapperImpl implements OrdenDeTrabajoMaquinaMap
                 .maquinaId(entity.getMaquina() != null ? entity.getMaquina().getId() : null)
                 .fechaInicio(entity.getFechaInicio())
                 .fechaFin(entity.getFechaFin())
-                .estado(entity.getEstado())
+                .estado(entity.getEstado().name())
                 .observaciones(entity.getObservaciones())
                 .build();
     }
